1ff2aa4913b4da7f50991b8763da3f7620203a7a
J1min <aoiharu41@gmail.com>
2023-07-03 11:26:03 +0900

    헤더 레이아웃 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { HamburgerIcon, PlusIcon } from "@/Icon";
 
 export default function Header() {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 text-gray-200 border-b border-white/20">
       <div className="flex justify-between pt-1 px-3">
         <button className="-ml-0.5 -mt-0.5 inline-flex h-10 w-10 items-center justify-center rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white dark:hover:text-white">
           <HamburgerIcon className="w-6 h-6" />
